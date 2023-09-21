8fb128ae5ed3cdeeeea102829ec381a5d88c5f71
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-21 16:33:20 +0530

    Update disease.py

--- routes/disease.py
+++ routes/disease.py
@@ -1,15 +1,7 @@
-from datetime import timedelta
-
 from typing import Annotated
 
 from fastapi import APIRouter, Form, HTTPException, status, Depends
 from fastapi.security import OAuth2PasswordRequestForm
-
-from models.user_model import UserInDB
-
-from auth.authorize import authenticate_user, oauth2_scheme
-from auth.hashing import get_password_hash, ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token, blacklist_token
-from services.user_service import user_exists, get_next_avail_id, add_new_user
 
 """
     routers for authentication
