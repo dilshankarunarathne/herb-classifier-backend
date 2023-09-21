2aafe8637566e8e2fbd53bb4678c5713c81f6eed
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-21 17:15:35 +0530

    Update herb.py

--- routes/herb.py
+++ routes/herb.py
@@ -4,4 +4,8 @@ from fastapi import APIRouter, Form, Depends
     routers for herbs
 """
 
-
+router = APIRouter(
+    prefix="/api/disease",
+    tags=["disease"],
+    responses={404: {"description": "The requested page was not found"}},
+)
