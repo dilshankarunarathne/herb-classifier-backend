872467f2b3becc1240e52103b72f9770520c71ab
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-21 21:56:30 +0530

    Update location_service.py

--- services/location_service.py
+++ services/location_service.py
@@ -4,7 +4,8 @@ dao = LocationDAO(host="localhost", user="root", password="", database="herb")
 dao.connect()
 
 
-def add_location(lon, lat, herb)
+def add_location(lon, lat, herb):
+    
 
 
 def get_location(herb):
