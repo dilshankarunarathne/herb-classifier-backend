1db2331e6a3253a6e663f1fac5013e5a11af5d03
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-21 17:07:09 +0530

    Update herb_dao.py

--- dao/herb_dao.py
+++ dao/herb_dao.py
@@ -2,5 +2,5 @@ import mysql.connector
 from mysql.connector import errorcode
 
 """
-    middleware for accessing the herb database and performing CRUD operations on the disease table
+    middleware for accessing the herb database and performing CRUD operations on the herbs table
 """
