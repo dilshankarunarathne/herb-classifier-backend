8a203a5a091b7170026bafdc3b69aca3af074061
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-21 17:18:13 +0530

    Update herb.py

--- routes/herb.py
+++ routes/herb.py
@@ -33,4 +33,4 @@ async def search_herb_by_disease(
     if get_current_user(token) is None:
         raise credentials_exception
 
-    return get_herb_by_disease(disease)
+    return get_disease_by_herb()
