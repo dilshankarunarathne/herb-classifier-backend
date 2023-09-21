cd9af7b8c637cbac2c0a4d8483fab5a147484503
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-21 21:59:59 +0530

    Update location_dao.py

--- dao/location_dao.py
+++ dao/location_dao.py
@@ -2,7 +2,7 @@ import mysql.connector
 from mysql.connector import errorcode
 
 """
-    middleware for accessing the herb database and performing CRUD operations on the herbs table
+    middleware for accessing the herb database and performing CRUD operations on the location table
 """
 
 
