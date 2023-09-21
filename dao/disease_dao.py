db988f24388d9984226998217ce58fbf3c2f3623
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-21 07:52:26 +0530

    Update disease_dao.py

--- dao/disease_dao.py
+++ dao/disease_dao.py
@@ -5,6 +5,7 @@ from mysql.connector import errorcode
     middleware for accessing the herb database and performing CRUD operations on the disease table
 """
 
+
 class DiseaseDAO:
     def __init__(self, host, user, password, database):
         self.host = host
