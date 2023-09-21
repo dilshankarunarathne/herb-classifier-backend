f51101fde5acc8a008de72a1831d75e3cda8d0c8
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-21 07:05:59 +0530

    Update main.py

--- classifier/main.py
+++ classifier/main.py
@@ -12,7 +12,6 @@ img_array = image.img_to_array(img)
 img_array = np.expand_dims(img_array, axis=0)
 img_array = img_array / 255.0  # Normalize pixel values to [0, 1]
 
-# Make predictions
 predictions = model.predict(img_array)
 
 # Assuming you have a list of class labels
