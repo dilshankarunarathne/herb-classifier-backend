d1af8bc00cc6aaa35ba6229559d4c8c6683edea7
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-21 17:13:01 +0530

    Update herb_service.py

--- services/herb_service.py
+++ services/herb_service.py
@@ -5,4 +5,8 @@ dao.connect()
 
 
 def get_disease_by_herb(herb):
-    
+    diseases = dao.get_all()
+    return_list = []
+    for d in diseases:
+        return_list.append(d[0])
+    return return_list
