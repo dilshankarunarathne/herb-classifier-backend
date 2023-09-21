c748bd17f5830afd94c0dd2abee8fb058d5c8f2f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-21 07:11:27 +0530

    Update main.py

--- classifier/main.py
+++ classifier/main.py
@@ -4,3 +4,10 @@ from tensorflow.keras.preprocessing import image
 
 
 def recognize_plant(image):
+    blob = cv2.dnn.blobFromImage(
+        image,
+        1.0,
+        (227, 227),
+        (78.4263377603, 87.7689143744, 114.895847746),
+        swapRB=False
+    )
