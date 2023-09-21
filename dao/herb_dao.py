7cc610e8c028a8a3f11f857647b61f44dd080eef
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-21 17:09:04 +0530

    Update herb_dao.py

--- dao/herb_dao.py
+++ dao/herb_dao.py
@@ -37,7 +37,7 @@ class HerbDAO:
     def get_disease(self, herb):
         try:
             cursor = self.cnx.cursor()
-            query = "SELECT * FROM herbs WHERE disease = %s"
+            query = "SELECT * FROM herbs WHERE herb = %s"
             cursor.execute(query, (herb, ))
             row = cursor.fetchone()
             cursor.close()
