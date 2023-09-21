4f0eef790c8a9eb3571732590aba1594d1afe7bd
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-21 16:33:33 +0530

    Update disease.py

--- routes/disease.py
+++ routes/disease.py
@@ -7,5 +7,9 @@ from fastapi.security import OAuth2PasswordRequestForm
     routers for authentication
 """
 
-
+router = APIRouter(
+    prefix="/api/auth",
+    tags=["auth"],
+    responses={404: {"description": "The requested page was not found"}},
+)
 
