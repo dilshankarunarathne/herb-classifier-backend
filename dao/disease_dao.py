f75489a902ebe0a03aa01d781cc504f8f4293e75
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-21 07:40:48 +0530

    Update disease_dao.py

--- dao/disease_dao.py
+++ dao/disease_dao.py
@@ -1,3 +1,3 @@
 """
-    middleware for accessing the disease database and performing CRUD operations on the disease table
+    middleware for accessing the herb database and performing CRUD operations on the disease table
 """
