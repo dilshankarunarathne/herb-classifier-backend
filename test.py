58ce28fdaff43fa5e04a8254356ad1935032beed
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-21 08:00:12 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ from dao.disease_dao import DiseaseDAO
 dao = DiseaseDAO(host="localhost", user="root", password="", database="herb")
 
 
-def get_disease_details:
-    
+def get_disease_details():
+    pass
