2c4808b40ae82ec04382785b8a0033883f59c948
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-21 08:16:46 +0530

    Update disease_service.py

---
+++ services/disease_service.py
@@ -0,0 +1,9 @@
+from dao.disease_dao import DiseaseDAO
+
+dao = DiseaseDAO(host="localhost", user="root", password="", database="herb")
+dao.connect()
+
+
+def get_disease_details(disease):
+    (disease_id, disease_name, symptoms, treatment) = dao.query_disease(disease)
+    return symptoms, treatment
