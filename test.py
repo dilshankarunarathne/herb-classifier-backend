6f508c7cdc3bf37e3bb4b9fb9b112f187fc430ca
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-21 08:21:53 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 from dao.disease_dao import DiseaseDAO
+from services.disease_service import get_all_diseases
 
 dao = DiseaseDAO(host="localhost", user="root", password="", database="herb")
 dao.connect()
 
 
-print(dao.get_all())
+print(get_all_diseases())
