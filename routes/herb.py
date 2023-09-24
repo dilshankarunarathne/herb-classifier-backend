83bc1c0cef724b6e345f85d763b976b21fb43d80
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-25 02:54:35 +0530

    Update herb.py

--- routes/herb.py
+++ routes/herb.py
@@ -10,7 +10,11 @@ from services.herb_service import get_herb_by_disease, get_disease_by_herb
         router (APIRouter): the router for the endpoint
 
     Methods:
-        
+        [POST] /api/herb/search-herbs
+        search_herb_by_disease: the endpoint for searching a herb by disease
+
+        [POST] /api/herb/search-diseases
+        search_herb_by_disease: the endpoint for searching a disease by herb
 
     Raises:
         HTTPException: if the user is not logged in
