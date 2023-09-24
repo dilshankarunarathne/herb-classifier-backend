58606e134caf36e33bf54178de3057ebaad21892
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-25 02:47:44 +0530

    Update location.py

--- routes/location.py
+++ routes/location.py
@@ -25,7 +25,7 @@ async def get_location_for_herb(
     The endpoint for getting location for a herb
     Args:
         herb (str): the herb to search for
-        token: the token for the user
+        token (): the token for the user
 
     Returns:
         The location for the herb
