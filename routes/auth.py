5dbdc2f1475014ed398c97bceaf2b51d9af63eca
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-25 02:26:13 +0530

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -33,6 +33,7 @@ async def register_user(
     The endpoint for registering a new user
     :param username: the username of the new user
     :param email: the email of the new user
+    :param password: the password of the new user
     """
     if user_exists(username):
         raise HTTPException(
