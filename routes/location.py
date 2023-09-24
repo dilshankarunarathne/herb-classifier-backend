f772f402b52cf9216da23ed1bbc49b0fd29e947f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-25 02:48:16 +0530

    Update location.py

--- routes/location.py
+++ routes/location.py
@@ -45,6 +45,7 @@ async def add_new_location(
     herb: str = Form(...),
     token: str = Depends(oauth2_scheme)
 ):
+    """
     if get_current_user(token) is None:
         raise credentials_exception
 
