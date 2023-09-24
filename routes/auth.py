7f7e33f77642a31d5fb1d8efbc4b0247b27e855c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-25 02:21:48 +0530

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -27,7 +27,8 @@ async def register_user(
         username: str = Form(...),
         email: str = Form(...),
         password: str = Form(...),
-        is_admin=False):
+        is_admin=False
+):
     if user_exists(username):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
