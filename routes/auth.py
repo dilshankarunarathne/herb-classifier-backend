ddd1fc0b254ee99809c7a2e5a7b251c79baec959
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-25 02:27:47 +0530

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -84,5 +84,6 @@ async def login_for_access_token(
 
 @router.post("/logout")
 async def logout(token: str = Depends(oauth2_scheme)):
+    
     blacklist_token(token)
     return {"message": "Successfully logged out"}
