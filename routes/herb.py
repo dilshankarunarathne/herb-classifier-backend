3080a8ce6fa3ee91f7a93b72c5e834dcf1699386
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-25 02:30:46 +0530

    Update herb.py

--- routes/herb.py
+++ routes/herb.py
@@ -21,7 +21,7 @@ async def search_herb_by_disease(
         disease: str = Form(...),
         token: str = Depends(oauth2_scheme)
 ):
-    
+    """
     if get_current_user(token) is None:
         raise credentials_exception
 
