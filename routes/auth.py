bbe3af42ebf2a03e39be8088ddf3bba1707e5277
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-25 02:27:32 +0530

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -63,6 +63,7 @@ async def login_for_access_token(
     The endpoint for logging in a user
     :param form_data: the form data for the login
     :return: the access token for the user
+    
     user = authenticate_user(form_data.username, form_data.password)
     if not user:
         raise HTTPException(
