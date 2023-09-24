b8f0598100a94e4f76a89f56310cf980384ede16
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-25 02:30:49 +0530

    Update herb.py

--- routes/herb.py
+++ routes/herb.py
@@ -22,6 +22,7 @@ async def search_herb_by_disease(
         token: str = Depends(oauth2_scheme)
 ):
     """
+    The endpoint for searching a herb by disease
     if get_current_user(token) is None:
         raise credentials_exception
 
