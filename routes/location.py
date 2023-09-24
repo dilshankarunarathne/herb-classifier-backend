850a120f93cd7ab5d5feac79d6ff305f9ad281a3
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-25 02:31:47 +0530

    Update location.py

--- routes/location.py
+++ routes/location.py
@@ -24,6 +24,7 @@ async def get_location_for_herb(
     """
     The endpoint for getting location for a herb
     :param herb: the herb to search for
+    :param token: the token of the user
     if get_current_user(token) is None:
         raise credentials_exception
 
