b9434db41a2ca917df4e6f7ba762a8ce7d8cd4e5
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-25 02:56:16 +0530

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -50,6 +50,7 @@ async def register_user(
         email (str): the email of the user
         password (str): the password of the user
         is_admin (bool): whether the user is an admin
+        
     """
     if user_exists(username):
         raise HTTPException(
