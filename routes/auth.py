2e99a3be09214b7733f9fd6cee7e3d8ffd94afb7
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-25 02:57:24 +0530

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -88,6 +88,7 @@ async def login_for_access_token(
         (dict) The access token for the user
 
     Raises:
+        HTTPException: if the username or password is incorrect
     """
     user = authenticate_user(form_data.username, form_data.password)
     if not user:
