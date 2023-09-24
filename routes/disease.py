fc66897ae364c0d0081c5392f0a5d9ae93481b9d
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-25 02:29:23 +0530

    Update disease.py

--- routes/disease.py
+++ routes/disease.py
@@ -20,7 +20,7 @@ async def search_disease(
     token: str = Depends(oauth2_scheme)
 ):
     """
-    
+    The endpoint for searching a disease
     if get_current_user(token) is None:
         raise credentials_exception
 
