5e6db7cc76ebc5912ebe8f95522cd0309a55adf4
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-25 08:30:55 +0530

    Update hashing.py

--- auth/hashing.py
+++ auth/hashing.py
@@ -9,7 +9,7 @@ from services.token_service import add_token_to_blacklist, check_if_token_is_bla
     middleware for hashing passwords and creating tokens
 """
 
-SECRET_KEY = "3ac975a63c346504ccc4bad65505c619a2cc502b01c7a92e3288f4d3b0def92b"
+SECRET_KEY = 
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
