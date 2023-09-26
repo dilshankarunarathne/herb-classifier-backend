b92a0032ab2714f4d63c3ae199470aaf66d563fa
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-27 01:00:03 +0530

    Update disease.py

--- routes/disease.py
+++ routes/disease.py
@@ -41,6 +41,7 @@ async def insert_disease(
     Args:
        disease (str): the disease to search for
        token (oauth2 bearer token): the token for the user
+       
 
     Returns:
        (str) Successfully added new disease details record
