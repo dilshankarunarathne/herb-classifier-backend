13f762826e21451548a10c6120c495d6187eb29c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-27 02:14:06 +0530

    Update location_service.py

--- services/location_service.py
+++ services/location_service.py
@@ -18,5 +18,5 @@ def get_location(herb):
     herbs = dao.get_location_by_herb(herb)
     return_list = []
     for h in herbs:
-        return_list.append({h[1], h[2]})
+        return_list.append({h[1], h[2], h{4}})
     return return_list
