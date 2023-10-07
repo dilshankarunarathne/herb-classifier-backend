a5bd840e414f2cb524959a0729d01c951fa8061e
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-07 16:58:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ from fastapi import FastAPI
 
 from routes import auth, disease, herb, location
 
+origins = ['http://localhost:3000','http://192.168.178.23:3000']
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 
