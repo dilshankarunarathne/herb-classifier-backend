613c261480a8aa5515eff0bcaca8254c0eddc93f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-14 10:15:21 +0530

    Update __init__.py

--- pyes/__init__.py
+++
@@ -1,4 +0,0 @@
-from pyswip import Prolog
-prolog = Prolog()
-prolog.assertz("father(john, jim)")  # Sample query to check if pyswip is working
-list(prolog.query("father(john, X)"))  # Should return [{'X': 'jim'}]
