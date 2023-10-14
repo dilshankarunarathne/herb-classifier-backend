2cb69a0c94cfa31852dc8097c2fad8d01a9032a8
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-14 10:04:15 +0530

    Update test.py

---
+++ pyes/test.py
@@ -0,0 +1,30 @@
+from pyswip import Prolog
+
+prolog = Prolog()
+prolog.consult("path/to/your/knowledge_base.pl")  # Replace with the actual path to your Prolog file
+
+
+@app.get("/diagnose/{symptom}")
+async def diagnose(symptom: str):
+    try:
+        # Query the Prolog knowledge base for the disease related to the given symptom
+        query = f"has_symptom(Disease, {symptom})"
+        diseases = list(prolog.query(query))
+
+        if diseases:
+            # Assuming there's only one disease for the given symptom
+            disease = diseases[0]['Disease']
+            # Query the Prolog knowledge base for the treatment of the diagnosed disease
+            treatment_query = f"treatment({disease}, Treatment)"
+            treatments = list(prolog.query(treatment_query))
+
+            if treatments:
+                treatment = treatments[0]['Treatment']
+            else:
+                treatment = "No specific treatment found."
+
+            return {"disease": disease, "treatment": treatment}
+        else:
+            return {"error": "No disease found for the given symptom."}
+    except Exception as e:
+        return {"error": str(e)}
