578a97ec7bab1973e0422c192c0bb614c3abc0a6
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-14 10:21:13 +0530

    Update esystem.py

--- routes/esystem.py
+++ routes/esystem.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Form, Depends
 
 from auth.authorize import oauth2_scheme, get_current_user, credentials_exception
 from services.disease_service import get_disease_details, add_disease
+from services.es_service import diagnose_by_disease
 
 """
     API router for esystem (expert system) endpoint
