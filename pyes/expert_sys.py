f4fbab333e07a15289423c7e78c4073eed47b557
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-16 22:50:08 +0530

    Update expert_sys.py

--- pyes/expert_sys.py
+++ pyes/expert_sys.py
@@ -59,7 +59,7 @@ def translate_to_prolog_query(verbal_query):
     return query_mappings.get(verbal_query, "Unknown query.")
 
 
-def query_knowledge_base()
+def query_knowledge_base(query: str)
 user_query = input("Please enter your query: ")
 
 prolog_query = translate_to_prolog_query(user_query)
