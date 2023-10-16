2f9e30def49f66262492eb8a31eb015aa98e77f0
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-16 22:29:36 +0530

    Update test.py

--- pyes/test.py
+++ pyes/test.py
@@ -8,7 +8,8 @@ prolog.consult("knowledge_base.pl")
 query_mappings = {
     "What are the symptoms of pain in the heart?": "has_symptom(pain_in_heart, Symptom).",
 "What are the common symptoms of semgedi?": "has_common_symptom(semgedi, Symptom).",
-"How severe is pain_in_heart?": 
+"How severe is pain_in_heart?": "severity_level(pain_in_heart, Severity).",
+    
     # Add more query mappings as needed
 }
 
