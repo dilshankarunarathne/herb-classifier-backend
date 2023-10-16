951f203db616fbd373a58d975f3e0c2be983692c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-16 22:50:23 +0530

    Update expert_sys.py

--- pyes/expert_sys.py
+++ pyes/expert_sys.py
@@ -60,7 +60,6 @@ def translate_to_prolog_query(verbal_query):
 
 
 def query_knowledge_base(query: str):
-
 prolog_query = translate_to_prolog_query(user_query)
 
 results = list(prolog.query(prolog_query))
