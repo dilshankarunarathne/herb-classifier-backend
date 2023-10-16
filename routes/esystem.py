c65aec2b3b17c4d25f95b812c423d5a0e53d48c6
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-16 22:55:32 +0530

    Update esystem.py

--- routes/esystem.py
+++ routes/esystem.py
@@ -31,4 +31,4 @@ async def search_disease(
     if await get_current_user(token) is None:
         raise credentials_exception
 
-    return query_knowledge_base(disease)
+    return query_knowledge_base(query)
