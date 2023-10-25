382935f3333101fd7e0f1d4bf72fb39c7adcbf75
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-25 11:09:14 +0530

    Update expert_sys.py

--- pyes/expert_sys.py
+++ pyes/expert_sys.py
@@ -63,7 +63,11 @@ def translate_to_prolog_query(verbal_query):
     return query_mappings.get(verbal_query, "Unknown query.")
 
 
-
+def remove_special_chars(input_str: str) -> str:
+    input_str = input_str.replace('\n', '')  # Remove newline characters
+    input_str = input_str.replace(':', '')  # Remove colons
+    input_str = input_str.replace('_', ' ')  # Replace underscores with spaces
+    return input_str
 
 
 def process_user_query(query: str):
